485a210d3240518be3d3ffc125d629af28e8bd19
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-04-30 12:40:02 +0100

    Added UrlParamAndJWTIndexIsExact middleware to chi http middleware.

--- middlewares/http/chi/authorization.go
+++ middlewares/http/chi/authorization.go
@@ -1,14 +1,26 @@
 package chi
 
+import (
+	"net/http"
+
+	httpMd "github.com/Meghee/kit/middlewares/http"
+	"github.com/go-chi/chi"
+)
+
 // UrlParamAndJWTIndexIsExact checks if the value a url param is exactly the same as
 // the value a the jwt payload index.
 //
 // Note: the jwt token is gotten from the authorization bearer header.
-// func UrlParamAndJWTIndexIsExact(param, jwtIndex string, notAuthorizedHandler http.HandlerFunc) func(next http.Handler) http.Handler {
-// 	return func(next http.Handler) http.Handler {
-// 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 			jwtToken, jwtPayload, err := httpMd.RetrieveJWTFromHTTPHeader(w, r, notAuthorizedHandler)
-// 			if err
-// 		})
-// 	}
-// }
+func UrlParamAndJWTIndexIsExact(param, jwtIndex string, notAuthorizedHandler http.HandlerFunc) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			jwtPayload := r.Context().Value(httpMd.JWTPayloadCtxKey).(map[string]interface{})
+			paramValue := chi.URLParam(r, param)
+			if jwtPayload[jwtIndex] != paramValue {
+				notAuthorizedHandler(w, r)
+				return
+			}
+			next.ServeHTTP(w, r.WithContext(r.Context()))
+		})
+	}
+}

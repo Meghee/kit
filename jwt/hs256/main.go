f6f18e27192279a2f612369b9577e7a07dd9891c
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-04-30 06:43:03 +0100

    Added Encode and Decode function to hs256 jwt package.

---
+++ jwt/hs256/main.go
@@ -0,0 +1,42 @@
+package hs256
+
+import (
+	"errors"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// Encode encodes a jwt token using data gotten from payload.
+func Encode(payload map[string]interface{}, secretKey []byte) (tokenString string, err error) {
+	claims := jwt.MapClaims{
+		"iat": time.Now(),
+	}
+	for k, v := range payload {
+		claims[k] = v
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err = token.SignedString(secretKey)
+	return
+}
+
+// Decode decodes a jwt token string.
+//
+// If the jwt token is invalid it returns an error.
+func Decode(tokenString string, secretKey []byte) (claims map[string]interface{}, err error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return "", errors.New("Invalid jwt token string")
+		}
+		return secretKey, nil
+	})
+	if err != nil {
+		return
+	}
+	if token.Valid {
+		claims = token.Claims.(jwt.MapClaims)
+		return
+	}
+	err = errors.New("An unknowm error occured while decoding jwt")
+	return
+}
